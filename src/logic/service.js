109749f026357a429a2ac895d8502f20e0d25dce
José Andy Echevarría Contreras <andyechevarria0522@gmail.com>
2023-05-30 00:14:02 +0200

    ...

---
+++ src/logic/service.js
@@ -0,0 +1,5 @@
+export const API_KEY = 'wwiebR1AY04wlxvTtZto8-q0KFY8tC-1cp_YHXjs98A'
+
+export function ENDPOINT_API_IMG_URL(query) {
+  return (`https://api.unsplash.com/search/photos?query=${query}&client_id=`)
+}

8dca65e53fe8ea5b086dd6a048264c30693d33b0
José Andy Echevarría Contreras <andyechevarria0522@gmail.com>
2023-05-30 00:43:41 +0200

    .

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import './Logo.css'
 export function Logo() {
   return (
     <div className="ws-logo">
-      <span className="ws-logo-text">IS</span>
+      <span className="ws-logo-text">WS</span>
     </div>
   )
 }
